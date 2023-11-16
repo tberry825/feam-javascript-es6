51c3f23e63a4e108413e3a703899ae3c127beb94
tberry825 <tracief515@gmail.com>
2023-11-16 15:42:21 -0600

    resubmitting code

--- filter.js
+++ filter.js
@@ -1,6 +1,12 @@
 //You are given an array of numbers. Your task is to write a JavaScript function that uses the .filter() method to create a new array containing only the odd numbers from the original array.
 
 //Here's the array:
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-//Your function should filter out the even numbers, leaving only the odd ones in the new array. Finally, log the resulting array to the console.
 
+//Your function should filter out the even numbers, leaving only the odd ones in the new array. Finally, log the resulting array to the console.
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function filterOddNumbers(arr) {
+    return arr.filter(number => number % 2 !== 0);
+}
+
+console.log(filterOddNumbers(numbers));
--- mapLoop.js
+++ mapLoop.js
@@ -1,7 +1,7 @@
 //Create a new array where each number is increased by one using .map()
 let arr = [1, 2, 3, 4, 5, 6, 7];
 
-
+let newArr = arr.map(number => number + 1);
 
 //Exercise 2
 
@@ -11,5 +11,9 @@ Refactor this using map. log the new array to the console.*/
 const numbers = [65, 44, 12, 4];
 
 function myFunction(num) {
-  return num * 10;
+    return num * 10;
 }
+
+const newArray = numbers.map(myFunction);
+
+console.log(newArray);
